da797b5090c2223a9f710676efe1095c7d5cce08
uglyghost <chenxingyan@bupt.edu.cn>
2025-12-04 09:12:34 +0800

    v0.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/automation-course/', // ⚠️ 务必与 GitHub 仓库名一致，前后都要有斜杠
 })
