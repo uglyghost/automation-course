2a5cb00d32ee9536a1d38c0129d5d13a82e5316a
uglyghost <chenxingyan@bupt.edu.cn>
2025-12-04 18:27:54 +0800

    v0.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/automation-course/', // ⚠️ 务必与 GitHub 仓库名一致，前后都要有斜杠
+  // ⚠️ 注意：这里的名称必须与您的 GitHub 仓库名称完全一致！
+  // 例如您的仓库地址是 https://github.com/user/my-project
+  // 那么这里就填 '/my-project/'
+  base: '/industrial-llm-demo/',
 })
