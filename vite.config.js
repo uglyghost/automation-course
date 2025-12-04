1c456cf2c99f74b2cde1b5e7358c6665c1fdec42
uglyghost <chenxingyan@bupt.edu.cn>
2025-12-04 18:32:53 +0800

    v0.0.1

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   // ⚠️ 注意：这里的名称必须与您的 GitHub 仓库名称完全一致！
   // 例如您的仓库地址是 https://github.com/user/my-project
   // 那么这里就填 '/my-project/'
-  base: '/industrial-llm-demo/',
+  base: '/automation-course/',
 })
